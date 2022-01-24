2b7e7e7351d43f54c2fd67a44b299b52194b9101
Hungry Nomad <42802559+HungryNomad@users.noreply.github.com>
2022-01-24 14:02:36 +0000

    added view for member list and token count

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useEffect, useMemo, useState } from "react";
 
 import { useWeb3 } from "@3rdweb/hooks";
 import { ThirdwebSDK } from "@3rdweb/sdk";
+import { ethers } from "ethers";
 
 const sdk = new ThirdwebSDK("rinkeby");
 
 const bundleDropModule = sdk.getBundleDropModule(
   "0x5bE2C11CdbB7E9b5a82f1A56598d37d31F349452"
+);
+
+const tokenModule = sdk.getTokenModule(
+  "0xCa7c3f9a1E33D1F9359968AD7e2588F715423427"
 );
 
 const App = () => {
@@ -18,6 +23,61 @@ const App = () => {
   // Use isClaiming to keep a loading state while NFT is minting
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   const [isClaiming, setIsClaiming] = useState(false);
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+  const [memberAddresses, setMemberAddresses] = useState([]);
+  // Shorten the address
+  const shortenAddress = (str) => {
+    return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+  };
+
+  // Grab addresses of NFT holders
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+    bundleDropModule
+      .getAllClaimerAddresses("0")
+      .then((addresses) => {
+        console.log("Member addresses", addresses);
+        setMemberAddresses(addresses);
+      })
+      .catch((err) => {
+        console.error("Failed to get member list", err);
+      });
+  }, [hasClaimedNFT]);
+
+  // Grabs the # of token each member holds.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Grab all the balances.
+    tokenModule
+      .getAllHolderBalances()
+      .then((amounts) => {
+        console.log("Amounts", amounts);
+        setMemberTokenAmounts(amounts);
+      })
+      .catch((err) => {
+        console.error("failed to get token amounts", err);
+      });
+  }, [hasClaimedNFT]);
+
+  // Now, we combine the memberAddresses and memberTokenAmounts into a single array
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      return {
+        address,
+        tokenAmount: ethers.utils.formatUnits(
+          // If the address isn't in memberTokenAmounts, it means they don't
+          // hold any of our token.
+          memberTokenAmounts[address] || 0,
+          18
+        ),
+      };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
 
   useEffect(() => {
     sdk.setProviderOrSigner(signer);
@@ -61,14 +121,37 @@ const App = () => {
   }
 
   // Show DAO to members
-  if (hasClaimedNFT) {
-    return (
-      <div className="member-page">
-        <h1>DAO Member page</h1>
-        <p>Congrats on being a member, more to come later.</p>
+if (hasClaimedNFT) {
+  return (
+    <div className="member-page">
+      <h1>Travel DAO Member Page</h1>
+      <p>Thank you for being a member, more things to come.</p>
+      <div>
+        <div>
+          <h2>Member List</h2>
+          <table className="card">
+            <thead>
+              <tr>
+                <th>Address</th>
+                <th>Token Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {memberList.map((member) => {
+                return (
+                  <tr key={member.address}>
+                    <td>{shortenAddress(member.address)}</td>
+                    <td>{member.tokenAmount}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+};
 
   const mintNFT = () => {
     setIsClaiming(true);
